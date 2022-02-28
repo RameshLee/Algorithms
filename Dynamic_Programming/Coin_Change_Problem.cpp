772462135af07380485857e8e1ea8f42deb49b16
Ramesh Ramasamy Pandi <rameshtaker@gmail.com>
2022-02-28 00:13:45 -0500

    Update Coin_Change_Problem.cpp

--- Dynamic_Programming/Coin_Change_Problem.cpp
+++ Dynamic_Programming/Coin_Change_Problem.cpp
@@ -5,11 +5,196 @@
 
 #include <iostream>
 #include <vector>
+#include <math.h>
 
 using namespace std;
+class Queue
+{
+public:
+    vector< vector<int> > q;
+
+     void push(vector<int> value){
+       q.insert(q.end(),value);
+     }
+
+     vector<int> pop(){
+         vector<int> front = q[0];
+         q.erase(q.begin());
+         return front;
+     }
+
+     void print()
+     {
+        printf("q-contents(%d): ", (int)q.size());
+        for (int i = 0; i < (int)q.size(); i++)
+        {
+            printf(" ");
+            for (int j = 0; j < (int)q[i].size(); j++)
+                printf("%d ", q[i][j]);
+        }
+        printf("\n");
+     }
+
+};
+
+class MergeSort
+{
+public:
+    Queue queue;
+    vector<int> v;
+    int n;
+
+    void fill_input(int size, int input[])
+    {
+        n = size;
+        for (int i=0; i<n; i++)
+        {
+            v.clear();
+            v.push_back(input[i]);
+            queue.push(v);
+        }
+    }
+
+    void fill_input(int size, vector<int> input)
+    {
+        n = size;
+        for (int i=0; i<n; i++)
+        {
+            v.clear();
+            v.push_back(input[i]);
+            queue.push(v);
+        }
+    }
+
+    void merge_sort()
+    {
+        vector<int> ans;
+        vector<int> A; vector<int> B;
+        while (1)
+        {
+            A.clear(); B.clear();
+            A = queue.pop();
+            B = queue.pop();
+            queue.push(merge(A,B));
+            //queue.print();
+
+            if ((int)queue.q.size() == 1)
+                break;
+        }
+
+        for (int i=0; i<queue.q.size(); i++)
+            ans.push_back(queue.q[0][i]);
+
+    }
+
+    vector<int> merge(vector<int> A, vector<int> B)
+    {
+        vector<int> new_vec;
+        int left_index = 0;
+        int right_index = 0;
+
+        int initial_size_A = (int)A.size();
+        int initial_size_B = (int)B.size();
+
+        while(left_index < initial_size_A || right_index < initial_size_B)
+        {
+            if (left_index == initial_size_A)//(int)A.size() == 0)
+            {
+                new_vec.push_back(B[right_index++]);
+            }
+            else if (right_index == initial_size_B)//(int)B.size() == 0)
+            {
+                new_vec.push_back(A[left_index++]);
+            }
+            else if (A[left_index] < B[right_index])
+            {
+                new_vec.push_back(A[left_index++]);
+            }
+            else if (A[left_index] > B[right_index])
+            {
+                new_vec.push_back(B[right_index++]);
+            }
+            else if (A[left_index] == B[right_index])
+            {
+                new_vec.push_back(B[right_index++]);
+            }
+        }
+        return new_vec;
+    }
+};
+
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+
+
+
+        vector<int> vec;
+        for (int i=0; i<nums.size(); i++)
+            vec.push_back(nums[i]);
+
+        if (vec.size() > 1)
+        {
+            MergeSort MS;
+            MS.fill_input(vec.size(), vec);
+            MS.merge_sort();
+            vec = MS.queue.q[0];
+        }
+
+        int min_rep = 1 + (int)floor(nums.size()/3);
+        vector<int> output;
+        int prev_value = vec[0];
+        int count = 1;
+
+        if (vec.size() == 1)
+            output.push_back(vec[0]);
+        if (vec.size() == 2)
+        {
+            output.push_back(vec[0]);
+            if (vec[0] != vec[1])
+                output.push_back(vec[1]);
+        }
+        else
+            for (int i=1; i<vec.size(); i++)
+            {
+
+                if (vec[i] == prev_value)
+                {
+                    count++;
+                    if (count == min_rep)
+                        output.push_back(vec[i]);
+                }
+                else
+                {
+                    prev_value = vec[i];
+                    count = 1;
+                }
+
+            }
+
+        return output;
+
+    }
+};
 
 int main()
 {
+    Solution S;
+    vector<int> vec;
+    int arr[] = {3,2,3,3};
+    for (int i=0; i<4; i++)
+        vec.push_back(arr[i]);
+
+    for (int i:vec)
+        printf("%d \n", i);
+    vector<int> ans = S.majorityElement(vec);
+    for (int i=0; i<4; i++)
+        printf("%d \n", ans[i]);
+
+         // Iterating over array
+    int a[] = {0, 1, 2, 3, 4, 5};
+    for (int n : a)
+        std::cout << n << ' ';
 
     printf("Exited the program!");
     return 0;
