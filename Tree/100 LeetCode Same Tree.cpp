6a7fa64c8e20bdb5f5ff972bfd5d65557ea1eb6b
Ramesh Ramasamy Pandi <rameshtaker@gmail.com>
2022-05-20 00:03:42 -0400

    Updated code !

--- Tree/100 LeetCode Same Tree.cpp
+++ Tree/100 LeetCode Same Tree.cpp
@@ -11,6 +11,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// 1) recursive approach
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
@@ -18,5 +20,31 @@ public:
         if (!p || !q) return false;
 
         return ((p->val == q->val) && isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
+    }
+};
+
+// 2) iterative approach
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (!p && !q) return true;
+
+        // bfs/level-order-traversal
+        queue<TreeNode*> q1, q2;
+        q1.push(p); q2.push(q);
+        while (!q1.empty()) {
+            auto a = q1.front(); q1.pop();
+            auto b = q2.front(); q2.pop();
+
+            if (!a && !b) continue;
+            if (!a || !b) return false;
+            if (a->val != b->val) return false;
+
+            if (a) q1.push(a->left);
+            if (a) q1.push(a->right);
+            if (b) q2.push(b->left);
+            if (b) q2.push(b->right);
+        }
+        return true;
     }
 };
